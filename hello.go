1c97195d2400401d265ee64631af0cd48e4ae06a
hosTheDev <hos.the.dev@gmail.com>
2024-12-27 16:05:55 +0530

    first get api

--- hello.go
+++ hello.go
@@ -1,9 +1,24 @@
 package main
 
 //std lib for input output etc.
-import "fmt"
+import (
+	"fmt"
+	"log"
+	//"fiber"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main(){
 	//print statments
 	fmt.Println("hello from go")
+
+	//to make a new instance of Fiber.
+	app := fiber.New()
+
+	app.Get("/get", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg":"Habibi"})
+	})
+
+	//to catch any errors
+	log.Fatal(app.Listen(":4000"))
 }
